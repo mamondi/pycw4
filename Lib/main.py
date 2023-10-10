68e37debdd4a820c8086c61fc2cfc621041278e8
DimamonDESKTOP <dimamon02@gmail.com>
2023-10-10 19:50:02 +0300

    updated

--- Lib/main.py
+++ Lib/main.py
@@ -1,66 +1,54 @@
 goods = ['Банан (Зелений)', 23.34, 'Полуниця', 45.56, 'Картопля молода', 15.99, 'Морква мита', 57.78]
 users = ['vvv@gmail.com', '1111']
-admins = ['vl@admin.com', 'qwerty'] #@admin.com
+admins = ['vl@admin.com', 'qwerty']
 basket = []
 admin_email_pattern = '@admin.com'
 
 is_login_true = False
 is_admin = False
 try:
+    with open('admins.txt', 'r') as admin_file:
+        admin_lines = admin_file.readlines()
+        admins = [line.strip().split(':') for line in admin_lines]
+
+    with open('users.txt', 'r') as user_file:
+        user_lines = user_file.readlines()
+        users = [line.strip().split(':') for line in user_lines]
+
+
     while True:
         while True:
-            login = input("Ведіть логін: ")
-            password = input("Ведіть пароль: ")
+            login = input("Введіть логін: ")
+            password = input("Введіть пароль: ")
+
             if admin_email_pattern in login:
-                if admins.count(login) == 0:
-                    print("Такого адміністратора не існує. Спробуйте ще раз")
-                    continue
-                if admins[admins.index(login) + 1] == password:
+                if [login, password] in admins:
                     print("Ви ввійшли як адміністратор")
                     is_admin = True
                     break
                 else:
-                    print("Пароль не вірний. Спробуйте ще раз")
-
-                    continue
-
-
-            elif login in users:
-                if users[users.index(login) + 1] == password:
-                    print(f"Ви ввійшли як користувач - {login}")
-                    break
-                else:
-                    print("Пароль не вірний")
+                    print("Логін або пароль адміністратора не вірний. Спробуйте ще раз.")
+            elif [login, password] in users:
+                print(f"Ви ввійшли як користувач - {login}")
+                break
             else:
-                print("Користувача з таким логіном не існує")
-                print("Бажаєте зареєструватися?")
-                answer = input("Ведіть так або ні: ")
-                if answer == 'так':
-                    users.append(login)
-                    users.append(password)
-                    print("Ви успішно зареєструвались")
-                    print("Ви можете увійти в систему")
-
-        # Робота системи
+                print("Логін або пароль не вірний. Спробуйте ще раз.")
         while True:
             if is_admin == True:
-                print('Ви ввійшли як адміністратор')
-                while True:
                     print('Для виходу введіть "exit"')
                     print('Для додавання товару введіть "add"')
                     print('Для видалення товару введіть "del"')
-                    print('Для виходу від адміністратора введіть "exit admin"')
-                    answer = input("Ведіть команду: ")
+                    answer = input("Введіть команду: ")
                     if answer == 'exit':
                         exit()
-                    elif answer == 'add':
-                        name = input("Ведіть назву товару: ")
-                        price = input("Ведіть ціну товару: ")
+                    elif answer == "add":
+                        name = input("Введіть назву товару: ")
+                        price = input("Введіть ціну товару: ")
                         goods.append(name)
                         goods.append(price)
                         print("Товар додано")
-                    elif answer == 'del':
-                        name = input("Ведіть назву товару: ")
+                    elif answer == "del":
+                        name = input("Введіть назву товару: ")
                         if name in goods:
                             goods.remove(name)
                             goods.remove(goods[goods.index(name) + 1])
@@ -75,9 +63,10 @@ try:
                 for i in range(0, len(goods), 2):
                     counter += 1
                     print(f"{counter} : {goods[i]}\t\t{goods[i + 1]}$")
-                print('Який продукт бажаєте купити?')
+                    print('Який продукт бажаєте купити?')
                 print('Для виходу введіть "exit"')
                 answer = input("Ведіть назву продукту: ")
+
                 if answer == 'exit':
                     print('Бажаєте вийти з програми?')
                     answer = input("Ведіть так або ні: ")
@@ -86,7 +75,6 @@ try:
                         exit()
                     else:
                         break
-
                 else:
                     if answer in goods:
                         basket.append(answer)
@@ -94,9 +82,6 @@ try:
                     else:
                         print("Такого продукту не існує")
                         continue
-            break
-
-
 
 except Exception as e:
     print(e)
